7321b07291014832e08b5dcf17fe833571b77ffd
Andrew Hunter <andrew.hunter@red-gate.com>
2014-03-18 10:54:27 +0000

    Add representation of an 'in flight' event

---
+++ Regard.Consumer.Logic/Api/IRegardEvent.cs
@@ -0,0 +1,48 @@
+﻿namespace Regard.Consumer.Logic.Api
+{
+    /// <summary>
+    /// Fluent, immutable API representing an event in transit
+    /// </summary>
+    public interface IRegardEvent
+    {
+        /// <summary>
+        /// The raw data for this event. This should be a JSON encoded string, or null if no raw data has been extracted from this event yet.
+        /// </summary>
+        string RawData { get; }
+
+        /// <summary>
+        /// The payload for this event. This should be null if no payload has been extracted yet, or a JSON encoded string.
+        /// </summary>
+        string Payload { get; }
+
+        /// <summary>
+        /// The organization for this event. This should be null if no organisation is known yet, or a JSON encoded string
+        /// </summary>
+        string Organization { get; }
+
+        /// <summary>
+        /// The product for this event. This should be null if no organisation is known yet, or a JSON encoded string
+        /// </summary>
+        string Product { get; }
+
+        /// <summary>
+        /// Event with some raw data
+        /// </summary>
+        IRegardEvent WithRawData(string data);
+
+        /// <summary>
+        /// New event with a payload
+        /// </summary>
+        IRegardEvent WithPayload(string data);
+
+        /// <summary>
+        /// New event with an organisation
+        /// </summary>
+        IRegardEvent WithOrganisation(string data);
+
+        /// <summary>
+        /// New event with a product
+        /// </summary>
+        IRegardEvent WithProduct(string data);
+    }
+}
---
+++ Regard.Consumer.Logic/RegardEvent.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Regard.Consumer.Logic.Api;
+
+namespace Regard.Consumer.Logic
+{
+    /// <summary>
+    /// C
+    /// </summary>
+    public class RegardEvent : IRegardEvent
+    {
+        private RegardEvent()
+        {
+            RawData         = null;
+            Payload         = null;
+            Organization    = null;
+            Product         = null;
+        }
+
+        /// <summary>
+        /// Creates a new event with some raw data
+        /// </summary>
+        public static IRegardEvent Create(string rawData)
+        {
+            return new RegardEvent().WithRawData(rawData);
+        }
+
+        private IRegardEvent With(Action<RegardEvent> updateEvent)
+        {
+            var newEvent = new RegardEvent();
+
+            newEvent.RawData        = RawData;
+            newEvent.Payload        = Payload;
+            newEvent.Organization   = Organization;
+            newEvent.Product        = Product;
+
+            updateEvent(newEvent);
+            return newEvent;
+        }
+
+        /// <summary>
+        /// The organization for this event. This should be null if no organisation is known yet, or a JSON encoded string
+        /// </summary>
+        public string Organization
+        {
+            get; private set;
+        }
+
+        /// <summary>
+        /// The payload for this event. This should be null if no payload has been extracted yet, or a JSON encoded string.
+        /// </summary>
+        public string Payload
+        {
+            get; private set;
+        }
+
+        /// <summary>
+        /// The product for this event. This should be null if no organisation is known yet, or a JSON encoded string
+        /// </summary>
+        public string Product
+        {
+            get; private set;
+        }
+
+        /// <summary>
+        /// The raw data for this event. This should be a JSON encoded string, or null if no raw data has been extracted from this event yet.
+        /// </summary>
+        public string RawData
+        {
+            get; private set;
+        }
+
+        /// <summary>
+        /// New event with an organisation
+        /// </summary>
+        public IRegardEvent WithOrganisation(string data)
+        {
+            return With((newEvt) => newEvt.Organization = data);
+        }
+
+        /// <summary>
+        /// New event with a payload
+        /// </summary>
+        public IRegardEvent WithPayload(string data)
+        {
+            return With((newEvt) => newEvt.Payload = data);
+        }
+
+        /// <summary>
+        /// New event with a product
+        /// </summary>
+        public IRegardEvent WithProduct(string data)
+        {
+            return With((newEvt) => newEvt.Product = data);
+        }
+
+        /// <summary>
+        /// Event with some raw data
+        /// </summary>
+        public IRegardEvent WithRawData(string data)
+        { 
+            return With((newEvt) => newEvt.RawData = data);
+        }
+    }
+}
