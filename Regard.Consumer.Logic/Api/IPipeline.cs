d667f55fc1480ea03cb2161c408b7672554a8536
Andrew Hunter <andrew.hunter@red-gate.com>
2014-03-18 11:12:24 +0000

    Add a representation of a pipeline and the pipeline processor

---
+++ Regard.Consumer.Logic/Api/IPipeline.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Regard.Consumer.Logic.Api
+{
+    /// <summary>
+    /// Represents a series of operations on an event
+    /// </summary>
+    public interface IPipeline
+    {
+        /// <summary>
+        /// The stages in this pipeline
+        /// </summary>
+        IEnumerable<IPipelineStage> Stages { get; }
+    }
+}
---
+++ Regard.Consumer.Logic/Pipeline/ProcessPipeline.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using Regard.Consumer.Logic.Api;
+
+namespace Regard.Consumer.Logic.Pipeline
+{
+    /// <summary>
+    /// Utility methods for processing a pipeline
+    /// </summary>
+    public static class ProcessPipeline
+    {
+        /// <summary>
+        /// Runs an event through a pipeline
+        /// </summary>
+        public static async Task<IRegardEvent> Process(this IPipeline pipeline, IRegardEvent input)
+        {
+            if (input == null)
+            {
+                return RegardEvent.Create(null).WithError("No input event");
+            }
+
+            var currentEvent = input;
+            foreach (var stage in pipeline.Stages)
+            {
+                currentEvent = await stage.Process(currentEvent);
+
+                if (currentEvent == null)
+                {
+                    // Oops, broken stage
+                    return RegardEvent.Create(null).WithError("Pipeline stage returned null");
+                }
+                else if (!string.IsNullOrEmpty(currentEvent.Error))
+                {
+                    return currentEvent;
+                }
+            }
+
+            return currentEvent;
+        }
+    }
+}
