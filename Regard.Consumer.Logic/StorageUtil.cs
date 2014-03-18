a028c2ccc442a166e98f24eb2bf1835ed6882fa4
Andrew Hunter <andrew.hunter@red-gate.com>
2014-03-18 17:42:46 +0000

    Need to sanitise the partition and row keys so they can't contain invalid characters so we don't get a 400 bad request.

---
+++ Regard.Consumer.Logic/StorageUtil.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Regard.Consumer.Logic
+{
+    static class StorageUtil
+    {
+        private const string c_InvalidChars = @"([\/\\\#\?\-]|[\x00-\x1f]|[\x7f-\x9f])";
+
+        /// <summary>
+        /// Replaces 'invalid' characters from a partition or row key. See http://msdn.microsoft.com/en-us/library/windowsazure/dd179338.aspx for
+        /// details on what constitutes an invalid character.
+        /// <para/>
+        /// Replacements consist of the string '-xx-' where 'xx' is the hexadecimal value of the invalid character. '-' is added as an invalid character
+        /// to ensure that the results are unique.
+        /// </summary>
+        public static string SanitiseKey(string key)
+        {
+            return Regex.Replace(key, c_InvalidChars, match =>
+                {
+                    StringBuilder result = new StringBuilder();
+
+                    result.Append('-');
+                    foreach (var matchChar in match.Value)
+                    {
+                        result.Append(((int) matchChar).ToString("x"));
+                    }
+                    result.Append('-');
+
+                    return result.ToString();
+                });
+        }
+    }
+}
