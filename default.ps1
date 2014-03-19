229ee5f7df0590a29e9582cae14093428972426c
Kevin Boyle <kevin.boyle@red-gate.com>
2014-03-19 10:28:07 +0000

    Start using psake to build

---
+++ default.ps1
@@ -0,0 +1,36 @@
+properties {
+    $base_dir = resolve-path .
+    $build_dir = "$base_dir\build"
+    $source_dir = "$base_dir\"
+    $package_dir = "$base_dir\packages"
+    $framework_dir =  (Get-ProgramFiles) + "\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0"
+    $config = "release"
+}
+
+framework('4.5.1')
+
+task default -depends package
+
+task compile {
+    "Compiling"
+    "   Regard.Consumer.sln"
+    
+    exec { msbuild $base_dir\Regard.Consumer.sln /p:Configuration=$config /verbosity:minimal /tv:12.0 }
+}
+
+task package -depends compile {
+    "Packaging"
+    "   Regard.Consumer.sln"
+
+    exec { msbuild $base_dir\Regard.Consumer\Regard.Consumer.ccproj /t:Publish /p:Configuration=$config /verbosity:minimal /tv:12.0 }
+}
+
+function Get-ProgramFiles
+{
+    #TODO: Someone please come up with a better way of detecting this - Tried http://msmvps.com/blogs/richardsiddaway/archive/2010/02/26/powershell-pack-mount-specialfolder.aspx and some enums missing
+    #      This is needed because of this http://www.mattwrock.com/post/2012/02/29/What-you-should-know-about-running-ILMerge-on-Net-45-Beta-assemblies-targeting-Net-40.aspx (for machines that dont have .net 4.5 and only have 4.0)
+    if (Test-Path "C:\Program Files (x86)") {
+        return "C:\Program Files (x86)"
+    }
+    return "C:\Program Files"
+}
