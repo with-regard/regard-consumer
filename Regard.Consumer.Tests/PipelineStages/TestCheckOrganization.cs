131c04321426294a4952c2ce8f402433132128a0
Andrew Hunter <andrew.hunter@red-gate.com>
2014-03-21 14:17:19 +0000

    Add tests for the check organization stage

---
+++ Regard.Consumer.Tests/PipelineStages/TestCheckOrganization.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using Regard.Consumer.Logic;
+using Regard.Consumer.Logic.Api;
+using Regard.Consumer.Logic.Pipeline;
+
+namespace Regard.Consumer.Tests.PipelineStages
+{
+    [TestFixture]
+    public class TestCheckOrganization
+    {
+        [Test]
+        public async Task AlwaysAcceptWithRegard()
+        {
+            // The 'WithRegard' organisation should be accepted always (it's used for the health checks, so these will fail if it isn't)
+            var stage = new CheckOrganization(new TestTableTarget());
+            var input = RegardEvent.Create("Raw data shouldn't matter").WithOrganization("WithRegard");
+
+            var result = await stage.Process(input);
+
+            Assert.IsNullOrEmpty(result.Error());
+        }
+
+        [Test]
+        public async Task RejectUnknownOrganization()
+        {
+            // Organizations that don't exist shouldn't get events added to the table
+            var stage = new CheckOrganization(new TestTableTarget());
+            var input = RegardEvent.Create("Raw data shouldn't matter").WithOrganization("DoesntExist");
+
+            var result = await stage.Process(input);
+
+            Assert.IsNotNullOrEmpty(result.Error());
+        }
+    }
+}
+ 
