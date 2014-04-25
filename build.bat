f7c7191f6bdd955749133469a1fc7e9a30017437
Andrew Hunter <andrew.hunter@red-gate.com>
2014-04-25 17:10:12 +0100

    Oops, forgot to actually tell nuget where to look

--- build.bat
+++ build.bat
@@ -3,7 +3,9 @@
 rem Set NuGetSources to something else if you want to use dependencies from a different server
 if "%NuGetSources%"=="" set NuGetSources=https://www.nuget.org/api/v2
 
-.nuget\NuGet.exe install .nuget\packages.config -OutputDirectory packages
+echo Looking for nuget sources at: %NuGetSources%
 
-.nuget\NuGet.exe restore Regard.Consumer.sln
+.nuget\NuGet.exe install .nuget\packages.config -OutputDirectory packages -source "%NuGetSources%" 
+
+.nuget\NuGet.exe restore Regard.Consumer.sln -source "%NuGetSources%" 
 powershell.exe -NoProfile -ExecutionPolicy unrestricted -Command "& {Import-Module '.\packages\psake.*\tools\psake.psm1'; invoke-psake .\default.ps1 %*; if ($LastExitCode -ne 0) {write-host "ERROR: $LastExitCode" -fore RED; exit $lastexitcode} }"
