5521538031973d1ea29adef6ac1612f7d1f32546
Andrew Hunter <andrew.hunter@red-gate.com>
2014-05-01 15:46:53 +0100

    Implement the code necessary to take the event apart and pass it into the event recorder

--- Regard.Consumer.Logic/Pipeline/StoreQueryEngine.cs
+++ Regard.Consumer.Logic/Pipeline/StoreQueryEngine.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Regard.Consumer.Logic.Api;
 using Regard.Query;
 using Regard.Query.Api;
@@ -109,9 +111,94 @@ namespace Regard.Consumer.Logic.Pipeline
                 }
             }
 
-            // TODO: To log the event, we need a session ID
+            if (dataStore == null)
+            {
+                return input;
+            }
 
-            // TODO: We need to treat events that start a session specially
+            // Events logged by this system need a particular payload format
+            string payload = input.Payload();
+            if (string.IsNullOrEmpty(payload))
+            {
+                Trace.WriteLine("StoreQueryEngine: ignoring event with no payload");
+                return input;
+            }
+
+            // The payload must be formatted as JSON
+            JObject jsonPayload;
+            try
+            {
+                jsonPayload = JObject.Parse(payload);
+            }
+            catch (JsonException e)
+            {
+                Trace.WriteLine("StoreQueryEngine: ignoring event which is not JSON");
+                return input;
+            }
+
+            // It must contain a session ID (stored as 'session-id'), which should be a valid GUID
+            JToken sessionIdToken;
+
+            if (!jsonPayload.TryGetValue("session-id", out sessionIdToken))
+            {
+                Trace.WriteLine("StoreQueryEngine: ignoring event with no session ID");
+                return input;
+            }
+
+            if (sessionIdToken.Type != JTokenType.String)
+            {
+                Trace.WriteLine("StoreQueryEngine: ignoring event with a non-string session ID");
+                return input;
+            }
+
+            Guid sessionId;
+            if (!Guid.TryParse(sessionIdToken.Value<string>(), out sessionId))
+            {
+                Trace.WriteLine("StoreQueryEngine: ignoring event with a session ID that isn't a GUID");
+            }
+
+            // It may contain a user ID. It must contain a user ID in order to instantiate a session.
+            // User id is stored in 'session-id'
+            JToken  userIdToken;
+            Guid    userId = Guid.Empty;
+
+            if (jsonPayload.TryGetValue("user-id", out userIdToken))
+            {
+                if (userIdToken.Type == JTokenType.String)
+                {
+                    if (!Guid.TryParse(userIdToken.Value<string>(), out userId))
+                    {
+                        userId = Guid.Empty;
+                    }
+                }
+            }
+
+            // It may instruct us to start a new session
+            // Any event containing a 'new-session' and a 'user-id' field is treated this way
+            bool startNewSession = false;
+
+            if (userId != Guid.Empty && sessionId != Guid.Empty)
+            {
+                // Empty user ID would create a bad session
+                // Empty session ID means 'create an arbitrary session ID' which is not the behaviour we want
+
+                JToken newSessionToken;
+                if (jsonPayload.TryGetValue("new-session", out newSessionToken))
+                {
+                    startNewSession = true;
+                }
+            }
+
+            // We need to treat events that start a session specially
+            if (startNewSession)
+            {
+                // Implies that the user ID and session ID are valid (see code above)
+                await dataStore.EventRecorder.StartSession(input.Organization(), input.Product(), userId, sessionId);
+            }
+
+            // Log this event
+            // TODO: it's probably redundant to log the user info, session ID here so we could take it out of the logged payload
+            await dataStore.EventRecorder.RecordEvent(sessionId, jsonPayload);
 
             // We don't translate the event
             return input;
