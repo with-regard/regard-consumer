128ba3b177e2135663d06b775249235e8d9e5509
Andrew Hunter <andrew.hunter@red-gate.com>
2014-05-01 16:06:45 +0100

    Start storing data using the default data storage engine

--- Regard.Consumer.BusWorker/WorkerRole.cs
+++ Regard.Consumer.BusWorker/WorkerRole.cs
@@ -81,7 +81,7 @@ namespace Regard.Consumer.BusWorker
             var customerTable               = new AzureFlatTableTarget(storageConnectionString, customerTableName);
 
             // For now we're just storing the data in the table
-            m_EventPipeline = new AzureTablePipeline(eventTable, customerTable, healthCheckSecret);
+            m_EventPipeline = new DefaultDataStorePipeline(eventTable, customerTable, healthCheckSecret);
 
             // Create the queue if it does not exist already
             string serviceBusConnectionString   = CloudConfigurationManager.GetSetting("Regard.ServiceBus.ConnectionString");
---
+++ Regard.Consumer.Logic/Pipeline/DefaultDataStorePipeline.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Regard.Consumer.Logic.Api;
+
+namespace Regard.Consumer.Logic.Pipeline
+{
+    /// <summary>
+    /// Pipeline that stores data in the Azure table as well as the default Data Storage event recorder
+    /// </summary>
+    public class DefaultDataStorePipeline : IPipeline
+    {
+        private readonly IPipelineStage[] m_Stages;
+
+        /// <summary>
+        /// Creates a new table pipeline
+        /// </summary>
+        public DefaultDataStorePipeline(IFlatTableTarget eventTable, IFlatTableTarget customerTable, string healthCheckSecret)
+        {
+            var decompose       = new DecomposeStage();
+            var checkSize       = new CheckDataSize();
+            var checkOrg        = new CheckOrganization(customerTable);
+            var checkProduct    = new CheckProduct();
+            var checkUser       = new CheckUser();
+            var checkEvent      = new CheckEvent();
+            var healthCheck     = new HealthCheckRoutingStage(healthCheckSecret);
+            var storeInTable    = new StoreAzureTable(eventTable);
+            var storeForQuery   = new StoreQueryEngine();
+
+            m_Stages = new IPipelineStage[] { decompose, checkSize, checkOrg, checkProduct, checkUser, checkEvent, healthCheck, storeInTable, storeForQuery };
+        }
+
+        /// <summary>
+        /// The stages in this pipeline
+        /// </summary>
+        public IEnumerable<IPipelineStage> Stages
+        {
+            get
+            {
+                return m_Stages;
+            }
+        }
+    }
+}
