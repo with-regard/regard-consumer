7033ed3d5e9754c9f50e988fd81160384db3979c
Andrew Hunter <andrew.hunter@red-gate.com>
2014-05-14 09:45:10 +0100

    Added a query endpoint to the consumer (this simplifies deployment and configuration management for the initial version over using a separate endpoint)

---
+++ Regard.Query.Internal.WebAPI/Startup.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Http;
+using Owin;
+
+namespace Regard.Query.Internal.WebAPI
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Configure for attribute routes
+            var httpConfiguration = new HttpConfiguration();
+            httpConfiguration.MapHttpAttributeRoutes();
+
+            app.UseWebApi(httpConfiguration);
+
+            app.Run(async context =>
+            {
+                // Default behaviour is is to just 404
+                context.Response.ContentType = "text/plain";
+                context.Response.StatusCode = 404;
+                await context.Response.WriteAsync("");
+            });
+        }
+    }
+}
